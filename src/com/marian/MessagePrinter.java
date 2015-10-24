3cacd071fc12d1bf6ae8915bf98ae7645537c400
Marian Sobczyk <m.sobczyk@nomtek.com>
2015-10-24 09:41:17 +0200

    Fixed printer and condition

--- src/com/marian/MessagePrinter.java
+++ src/com/marian/MessagePrinter.java
@@ -4,10 +4,11 @@ package com.marian;
  * Created by marian on 23.10.15.
  */
 public class MessagePrinter {
-    public static void printMessage(String s, byte[] decoded) {
+    public static void printMessage(String s, byte[] decoded, String breaker) {
         System.out.println(s);
         for (byte aDecoded : decoded) {
             System.out.print((char) aDecoded);
+            System.out.print(breaker);
         }
         System.out.println();
     }
--- src/com/marian/MessageValidator.java
+++ src/com/marian/MessageValidator.java
@@ -8,11 +8,7 @@ public class MessageValidator {
         boolean valid = true;
         for (int i = 0; i < decoded.length && valid; i++) {
             byte character = decoded[i];
-            if (character == 32 || (character >= 40 && character <= 125)) {
-                valid = true;
-            } else {
-                valid = false;
-            }
+            valid = character == 32 || (character >= 40 && character <= 125);
         }
 
         return valid;
